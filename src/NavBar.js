b9f0004afd5b9314ca5ca508d550f7f9ae14ef20
Deya Card <thedeyacard@gmail.com>
2023-01-23 14:24:19 -0800

    Add NavBar.js

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">
+            Surf's Up
+        </a>
+        <ul>
+            <li>
+                <a href="/rentals">Rentals</a>
+            </li>
+        </ul>
+    </nav>
+}
