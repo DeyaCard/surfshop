86440065b2577b9fbf3785445f5cba384c2499e8
Deya Card <thedeyacard@gmail.com>
2023-01-23 16:44:21 -0800

    Update main image - WIP

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Rentals from "./pages/Rentals"
 import Lessons from "./pages/Lessons"
 import About from "./pages/About"
 import Shop from "./pages/Shop"
+import mainImage from "./img/main.jpg"
 import { Route, Routes } from "react-router-dom"
 
 
 function App() {
   return (
     <>
+    
       <NavBar />
       <div className="container">
         <Routes>
@@ -21,6 +23,10 @@ function App() {
           <Route path="/shop" element={<Shop />} />
         </Routes>
       </div>
+
+      <div className="main-image">
+      <img src={mainImage} />
+    </div>
     </>
   )
 }
